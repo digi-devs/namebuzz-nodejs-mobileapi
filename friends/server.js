8112c06b7504285de28c17ae609091fc2d55cd4d
dp-devs <digiprayas@gmail.com>
2023-03-29 22:34:17 +0530

    db connection done

--- friends/server.js
+++ friends/server.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const app = express();
+const db = require('../api-gateway/db.js'); 
+const port = 3002;
 
-const port = 8083;
-
+// Apis
 app.get('/friends-list',(req,res)=> {
     // data from database
-    let response = {
-        data : {
-            item : [
-                {
-                    id:1,
-                    name:'friend 1'
-                },
-                {
-                    id:2,
-                    name:'friend 2'
-                },
-                {
-                    id:3,
-                    name:'friend 3'
-                },
-            ]
-        }
-    }
-    res.status(200).json(response)
+    db.query("SELECT * FROM user", function (err, result, fields) {
+        if (err) throw err;
+        res.status(200).json({err_code: "",
+        msg: "Posts added to response", data: result });
+      });
+
+    
+    
 })
 
 app.listen(port,()=>{
